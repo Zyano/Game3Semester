d95a0db45d57c31a593e728b353563ab6cbedf32
Mark Medum Bundgaard <mmedum@gmail.com>
2013-10-21 08:54:12 +0200

    Player is fixed with indents based on torbens strange code

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,55 +1,55 @@
 package game;
 
 public class Player {
-String name;
-int xpos;
-int ypos;
-int point;
-String direction;
+	String name;
+	int xpos;
+	int ypos;
+	int point;
+	String direction;
 
-public Player (String name) {
-	this.name = name;
-	xpos = 5;
-	ypos = 7;
-	point = 0;
-	direction = "up";
-}
-public int getPoint() {
-	return point;
-}
+	public Player (String name) {
+		this.name = name;
+		xpos = 5;
+		ypos = 7;
+		point = 0;
+		direction = "up";
+	}
+	public int getPoint() {
+		return point;
+	}
 
-public int getXpos() {
-	return xpos;
-}
+	public int getXpos() {
+		return xpos;
+	}
 
-public void setXpos(int xpos) {
-	this.xpos = xpos;
-}
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
 
-public int getYpos() {
-	return ypos;
-}
+	public int getYpos() {
+		return ypos;
+	}
 
-public void setYpos(int ypos) {
-	this.ypos = ypos;
-}
-public String getDirection() {
-	return direction;
-}
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+	public String getDirection() {
+		return direction;
+	}
 
-public void setDirection(String direction) {
-	this.direction = direction;
-}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
 
-String ToString () {
-	return name + "   " + point;
-}
-void addOnePoint() {
-	
-	point ++;
-}
+	String ToString () {
+		return name + "   " + point;
+	}
+	void addOnePoint() {
 
-void subOnePoint() {
-	point --;
-}
+		point ++;
+	}
+
+	void subOnePoint() {
+		point --;
+	}
 }
