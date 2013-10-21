f590149aa39fa83181562ecee87543a120c79c12
Stefan <Stefan.Weibel@gmail.com>
2013-10-21 11:43:16 +0200

    comments

--- src/server/ObjectInThread.java
+++ src/server/ObjectInThread.java
@@ -13,12 +13,22 @@ public class ObjectInThread extends Thread {
 	private ServerService service;
 	private String ip;
 	
+	/**
+	 *  Object stream used for receiving player objects.
+	 *  ip used as key for the object.
+	 * @param ois
+	 * @param ip
+	 */
 	public ObjectInThread(ObjectInputStream ois, String ip) {
 		this.ois = ois;
 		running = true;
 		service = ServerService.getInstance();
 	}
 
+	
+	/**
+	 * waits for input on the object stream if the object is of the type player then it's added/updated in collection on ServerService.
+	 */
 	public void run() {
 
 		while(running) {
