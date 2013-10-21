676b8d22a0f1a4bd95c34a2883db9149a0dfdf3f
Stefan <Stefan.Weibel@gmail.com>
2013-10-21 11:56:18 +0200

    Comments in the ServerService file!

--- src/serverService/ServerService.java
+++ src/serverService/ServerService.java
@@ -12,10 +12,19 @@ public class ServerService {
 	private static ServerService service;
 	private Map<InetAddress,Player> playerMap;
 	
+	/**
+	 * Creates the single ServerService instance. 
+	 * the creation of the concurrentHashMap happens here. 
+	 * The map uses InetAdress as key and Player as the value.
+	 */
 	public ServerService() {
 		playerMap = new ConcurrentHashMap<InetAddress, Player>();
 	}
 	
+	/**
+	 * creates an instance of the service class if non is currently in existence.
+	 * @return ServerService  
+	 */
 	public static ServerService getInstance() {
 		if(service == null) {
 			service = new ServerService();
@@ -23,14 +32,28 @@ public class ServerService {
 		return service;
 	}
 	
+	/**
+	 * adds a player to the map using the InetAdress as the key.
+	 * if a player is already saved by the key that player is overwritten.
+	 * @param ip
+	 * @param p1
+	 */
 	public void addPlayer(InetAddress ip, Player p1) {
 		playerMap.put(ip, p1);
 	}
 	
+	/**
+	 * remove the key and the underlying value from the map.
+	 * @param ip
+	 */
 	public void removePlayer(String ip) {
 		playerMap.remove(ip);
 	}
 	
+	/**
+	 * return a HashMap of the concurrentmap with the same keys and underlying values.
+	 * @return HashMap<Inetadress, Player>
+	 */
 	public HashMap<InetAddress, Player> getPlayerMap() {
 		return new HashMap<InetAddress,Player>(playerMap);
 	}
