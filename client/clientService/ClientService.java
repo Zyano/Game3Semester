3c6b2470a140f95d1583d2aafaf1dede66570bcf
Mark <mmedum@gmail.com>
2013-10-22 19:46:45 +0200

    We can send stuff

--- client/clientService/ClientService.java
+++ client/clientService/ClientService.java
@@ -7,20 +7,26 @@ import network.Connector;
 import model.Player;
 
 public class ClientService {
-	
-	private static final ClientService service = new ClientService();
-	private Connector connection;
-	
-	
+
+	private static ClientService service;
+	private static Connector connection; 
+
+
 	private ClientService() {
-		connection = new Connector("localhost", "8888");
+		
 	}
-	
+
 	/**
 	 * Returns an instance of the ClientService class used to for the basic communication between the GUI and the network layer.
 	 * @return ClientService
 	 */
-	public static ClientService getInstance() {
+	public static synchronized ClientService getInstance() {
+		synchronized (ClientService.class) {
+			if(service == null){
+				service = new ClientService();
+				connection = new Connector("localhost", "8888");
+			}
+		}
 		return service;
 	}
 
@@ -31,18 +37,16 @@ public class ClientService {
 	public void sendPlayerObject(Player p) {
 		connection.sendPlayer(p);
 	}
-	
+
 	/**
 	 * Update the UI with the newly received player list
 	 * TODO: create the method in the gui for updating the list and the methods called for making sure the scorelist is up to date.
 	 * @param list<player>
 	 */
 	public void updatePlayerList(List<Player> list) {
-		System.out.println("HGER");
-		
 		System.out.println(list);
 	}
-	
-	
-	
+
+
+
 }
